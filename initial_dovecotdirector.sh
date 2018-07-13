3a19efa116ddec9d75afe433d14336a9dafc0b61
Drizzt_Yin <drizzt.yin@cn.yieldnexus.com>
2018-07-13 14:08:27 +0800

    fix director bug

--- initial_dovecotdirector.sh
+++ initial_dovecotdirector.sh
@@ -6,6 +6,7 @@ cd / && mount -t efs fs-62f299cb:/ efs
 # Replace dovecot configure file & restart dovecot
 mv /etc/dovecot/dovecot.conf /etc/dovecot/dovecot.conf.bak
 cp /mnt/post_dovecot_demo/dovecot_director.conf /etc/dovecot/dovecot.conf
+cp /mnt/post_dovecot_demo/dovecot-bg-sql.conf.ext /etc/dovecot/
 
 mv /etc/dovecot/conf.d/10-director.conf mv /etc/dovecot/conf.d/10-director.conf.bak
 cd /mnt/post_dovecot_demo/ && cat 10-director.conf | perl -ne ' chomp($_);  $str = $_; $str =~ s/\[DIRECTOR_IPs\]/127.0.0.1/g; $str =~ s/\[BACKEND_IPs\]/127.0.0.1/g; print "$str\n";' > /etc/dovecot/conf.d/10-director.conf
