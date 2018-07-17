2cce927faf576b7f5c3423d19cc78f664012fe93
Drizzt_Yin <drizzt.yin@cn.yieldnexus.com>
2018-07-17 18:03:27 +0800

    environments with brands new vpc, subnets, nlb, efs

--- initial_dovecotdirector.sh
+++ initial_dovecotdirector.sh
@@ -1,7 +1,4 @@
 #NOTE:: This is a DOVECOT DIRECTOR CONFIG FILE!!!
-#Mount EFS
-#mkdir /efs
-#cd / && mount -t efs fs-62f299cb:/ efs
 
 # Replace dovecot configure file & restart dovecot
 mv /etc/dovecot/dovecot.conf /etc/dovecot/dovecot.conf.bak
